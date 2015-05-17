ec4c5ee8253d8a155b8e98aef535cd2531e1eeca
Alex Mayfield <alexmax2742@gmail.com>
2015-05-16 23:44:44 -0400

    Fix incorrect build step for global ACS.

--- build.bat
+++ build.bat
@@ -7,6 +7,10 @@ MKDIR build
 XCOPY wadsrc build /s /y
 CD build
 MKDIR acs
-..\bin\acc.exe scripts \GLOBAL.acs acs\GLOBAL.o
+..\bin\acc.exe scripts\GLOBAL.acs acs\GLOBAL.o
+if errorlevel 1 (
+	pause
+	exit /b 1
+)
 ..\bin\zip.exe -r ..\duel32%RELEASE%.pk3 .
 CD ..
